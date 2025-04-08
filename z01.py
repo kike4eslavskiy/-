c99d172229ffd7b05de40a8dd46b270ae841bb35
kike4eslavskiy <128180538+kike4eslavskiy@users.noreply.github.com>
2025-04-08 12:38:25 +0000

    z01.py

--- z01.py
+++ z01.py
@@ -1,9 +1,15 @@
-a = [1, 2, 3, 4, 5, 6, 7]
-min = a[0]
-min_old = min
-for x in a:
-    if min > x:
-        min_old=min
-        min=x
-
-print(min_old)
+array = [2, 1, 1, 2, 3, 5]
+min_1 = array[0]
+min_2 = array[1]
+for i in array:
+    if i < min_1:
+        min_2 = min_1
+        min_1 = i 
+    elif min_1 == min_2 != i:
+        min_2 = i
+    else:
+        if min_1 > i < min_2:
+            min_2 = i
+if min_1 == min_2:
+    print(f'Массив состоит из одинаковых элементов')
+print(min_1, min_2)
