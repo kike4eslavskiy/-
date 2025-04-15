b6aa5840ff24bb7e802a61781b85e9e259ad0e73
kike4eslavskiy <128180538+kike4eslavskiy@users.noreply.github.com>
2025-04-15 08:41:32 +0000

    z02.py

--- z02.py
+++ z02.py
@@ -2,15 +2,15 @@ import timeit
 import random
 
 def binary_search(arr, target): # Функция бинарного поиска: arr - массив, target - значение, которое надо найти
-    left, right = 0, len(arr) - 1 
+    left, right = 0, len(arr) - 1 #left первый элемент массива, а right последний элемент
     operations = 0
 
     while left <= right:
         operations += 1  # Считаем каждую операцию сравнения
-        mid = (left + right) // 2
+        mid = (left + right) // 2 # Индекс середины текущего диапазона поиска
         
-        if arr[mid] < target:
-            left = mid + 1
+        if arr[mid] < target: # Меньше ли индекс середины значения target.
+            left = mid + 1 
         elif arr[mid] > target:
             right = mid - 1
         else:
@@ -18,8 +18,7 @@ def binary_search(arr, target): # Функция бинарного поиска
     
     return -1, operations  # Возвращаем -1, если элемент не найден
 
-def fun():
-    # Создаём упорядоченный массив случайных чисел
+def fun(): # Создаём упорядоченный массив случайных чисел
     arr = sorted(random.sample(range(100, 800), 50))  # 50 уникальных чисел от 100 до 800
     target = random.randint(100, 800)  # Случайное значение для поиска
     
@@ -28,7 +27,7 @@ def fun():
     
     return index, operations
 
-start = timeit.default_timer()
+start = timeit.default_timer() # фиксируем время
 print("The start time is:", start)
 
 index, operations = fun()
