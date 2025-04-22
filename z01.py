17fb89bc26889fa1988297a22f7aea32d2bb619d
kike4eslavskiy <128180538+kike4eslavskiy@users.noreply.github.com>
2025-04-22 12:09:01 +0000

    z01.py

--- z01.py
+++ z01.py
@@ -1,6 +1,8 @@
+import time
 array = [2, 1, 1, 2, 3, 5]
 min_1 = array[0]  #Переменной присваивается первое значение из массива
 min_2 = array[1]
+start_time = time.time() # Записываем время начала выполнения программы
 for i in array:
     if i < min_1:
         min_2 = min_1
@@ -13,3 +15,6 @@ for i in array:
 if min_1 == min_2:
     print(f'Массив состоит из одинаковых элементов')
 print(min_1, min_2)
+end_time = time.time() # Записываем время окончания выполнения программы
+execution_time = end_time - start_time # Вычисляем и выводим время выполнения
+print(f'Время выполнения программы: {execution_time:.6f} секунд')
