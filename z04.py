3b074c274eecdf976f24d51d7a5674f719d0d4ae
kike4eslavskiy <128180538+kike4eslavskiy@users.noreply.github.com>
2025-04-23 01:52:24 +0000

    z04.py

--- z04.py
+++ z04.py
@@ -2,25 +2,25 @@ import heapq
 
 def dijkstra(graph, start):
      # Инициализация: расстояние до всех вершин - бесконечность, кроме стартовой
-    distances = {vertex: float('inf') for vertex in graph}
-    distances[start] = 0
+    distances = {vertex: float('inf') for vertex in graph} #Cоздает словарь distances, в котором инициализируются расстояния до всех вершин графа
+    distances[start] = 0 
      # Очередь с приоритетом (куча), храним (расстояние, вершину)
     priority_queue = [(0, start)]  # Приоритетная очередь (куча)
 
     while priority_queue:
-        current_distance, current_vertex = heapq.heappop(priority_queue)
+        current_distance, current_vertex = heapq.heappop(priority_queue) #Извлекаем вершину с наименьшим расстоянием из приоритетной очереди
 
         # Если текущее расстояние больше известного, пропускаем
         if current_distance > distances[current_vertex]:
             continue
 
         # Обновляем расстояния до соседей
-        for neighbor, weight in graph[current_vertex].items():
+        for neighbor, weight in graph[current_vertex].items(): #Cосед(neighbor) и вес ребра(weight), который соединяет current_vertex и neighbor
             distance = current_distance + weight
             # Если нашли более короткий путь до соседа, обновляем
-            if distance < distances[neighbor]:
+            if distance < distances[neighbor]: #Мы складываем текущее расстояние и вес ребра, чтобы получить полное расстояние от начальной вершины до соседа
                 distances[neighbor] = distance
-                heapq.heappush(priority_queue, (distance, neighbor))
+                heapq.heappush(priority_queue, (distance, neighbor)) #Добавляем соседа в приоритетную очередь с его обновленным расстоянием, чтобы позже его также обработать
 
     return distances
 
