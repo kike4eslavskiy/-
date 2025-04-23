c9c2c439aca683d69a354346aa977cd03b4cccd0
kike4eslavskiy <128180538+kike4eslavskiy@users.noreply.github.com>
2025-04-23 02:15:20 +0000

    stand_04.py

--- Stand4/stand_04.py
+++ Stand4/stand_04.py
@@ -1,24 +1,18 @@
-def dexter(graph, start):
-    distances = {vertex: float('infinity') for vertex in graph}
-    distances[start] = 0
-    visited = set() #Создание пустого множества visited, которое будет содержать вершины, которые уже были посещены
+import timeit
+from dexter import dexter
+
+def test():
+    graph = {} #Создает пустой словарь graph
     
-    while len(visited) < len(graph): #Начало цикла, который продолжается до тех пор, пока не будут посещены все вершины в графе
-        current_vertex = None
-        min_distance = float('infinity') #Инициализируем current_vertex как None, что будет использоваться для хранения текущей вершины. Инициализация min_distance как бесконечности, чтобы мы могли потом сравнить расстояния
-        for vertex in graph:
-            if vertex not in visited and distances[vertex] < min_distance: #Проверка, не была ли текущая вершина vertex ранее посещена и меньше ли её расстояние, чем min_distance.
-                min_distance = distances[vertex]
-                current_vertex = vertex
-        
-        if current_vertex is None:
-            break #Если current_vertex все еще None, это означает, что в графе нет достижимых вершин, и выполнение цикла может быть завершено
-        
-        visited.add(current_vertex)
-        
-        for neighbor, weight in graph[current_vertex].items():
-            distance = distances[current_vertex] + weight
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
+    start_time = timeit.default_timer() #Записывает текущее время в переменной start_time
+    result = dexter(graph, 'A') #Вызывает функцию dexter, передавая ей пустой граф graph и вершину A
+    elapsed_time = timeit.default_timer() - start_time #Измеряет время, прошедшее с момента запуска функции
     
-    return distances
+    print("Кратчайшие расстояния:")
+    for vertex, distance in result.items(): #Начинает цикл по элементам словаря result
+        print(f"{vertex}: {distance}")
+    
+    print(f"\nВремя выполнения: {elapsed_time:.6f} секунд")
+
+if name == "__main__": #Проверяет, выполняется ли данный модуль как основная программа.
+    test()
--- Stand4/stand_044.py
+++ Stand4/stand_044.py
@@ -2,17 +2,17 @@ import timeit
 from dexter import dexter
 
 def test():
-    graph = {} #Создает пустой словарь graph
+    graph = {'A': {'B': 1, 'C': 4}, 'B': {'A': 1, 'C': 2}, 'C': {'A': 4, 'B': 2}}
     
-    start_time = timeit.default_timer() #Записывает текущее время в переменной start_time
-    result = dexter(graph, 'A') #Вызывает функцию dexter, передавая ей пустой граф graph и вершину A
-    elapsed_time = timeit.default_timer() - start_time #Измеряет время, прошедшее с момента запуска функции
+    start_time = timeit.default_timer()
+    result = dexter(graph, 'A')
+    elapsed_time = timeit.default_timer() - start_time
     
     print("Кратчайшие расстояния:")
-    for vertex, distance in result.items(): #Начинает цикл по элементам словаря result
+    for vertex, distance in result.items():
         print(f"{vertex}: {distance}")
     
     print(f"\nВремя выполнения: {elapsed_time:.6f} секунд")
 
-if name == "__main__": #Проверяет, выполняется ли данный модуль как основная программа.
+if name == "__main__":
     test()
