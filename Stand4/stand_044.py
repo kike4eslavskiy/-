afd860d676f1796a97701bfacc522de133c520f1
kike4eslavskiy <128180538+kike4eslavskiy@users.noreply.github.com>
2025-04-23 02:09:14 +0000

    stand_044.py

--- Stand4/stand_044.py
+++ Stand4/stand_044.py
@@ -2,17 +2,17 @@ import timeit
 from dexter import dexter
 
 def test():
-    graph = {}
+    graph = {} #Создает пустой словарь graph
     
-    start_time = timeit.default_timer()
-    result = dexter(graph, 'A')
-    elapsed_time = timeit.default_timer() - start_time
+    start_time = timeit.default_timer() #Записывает текущее время в переменной start_time
+    result = dexter(graph, 'A') #Вызывает функцию dexter, передавая ей пустой граф graph и вершину A
+    elapsed_time = timeit.default_timer() - start_time #Измеряет время, прошедшее с момента запуска функции
     
     print("Кратчайшие расстояния:")
-    for vertex, distance in result.items():
+    for vertex, distance in result.items(): #Начинает цикл по элементам словаря result
         print(f"{vertex}: {distance}")
     
     print(f"\nВремя выполнения: {elapsed_time:.6f} секунд")
 
-if name == "__main__":
+if name == "__main__": #Проверяет, выполняется ли данный модуль как основная программа.
     test()
